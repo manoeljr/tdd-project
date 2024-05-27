5c35abcde8dccfc25871b2440af4cc2d11788950
Manoel <manoel.junior@tceal.tc.br>
2024-05-27 18:27:54 -0300

    feat: Configurando e adicionando testes para os schemas

---
+++ tests/schemas/test_product.py
@@ -0,0 +1,30 @@
+from uuid import UUID
+
+import pytest
+from pydantic import ValidationError
+
+from store.schemas.product import ProductIn
+from tests.factories import product_data
+
+
+def test_schema_validated_return_success():
+    data = product_data()
+    # product = ProductIn(**data)
+    product = ProductIn.validate(data)
+
+    assert product.name == "Iphone 14 pro Max"
+    assert isinstance(product.id, UUID)
+
+
+def test_schema_validated_return_failure():
+    data = {'name': "Iphone 14 pro Max", 'quantity': 10, 'price': 8.500}
+
+    with pytest.raises(ValidationError) as err:
+        ProductIn.validate(data)
+
+    assert err.value.errors()[0] == {
+        'type': 'missing',
+        'loc': ('status',),
+        'msg': 'Field required',
+        'input': {'name': 'Input'}
+    }
